21b1c75bd1b670d468cd65cff49abcb56e28f94c
jaafarhamza <jaafar.hamza711@gmail.com>
2025-12-09 19:26:59 +0100

    TRUC-15 Middleware authentification

--- Backend/routes/authRoutes.js
+++ Backend/routes/authRoutes.js
@@ -1,13 +1,19 @@
 import express from "express";
 import { register, login, getProfil } from "../controllers/authController.js";
+import {
+  registerValidation,
+  loginValidation,
+} from "../middlewares/validation.js";
+import { validate } from "../middlewares/validate.js";
+import { protect } from "../middlewares/auth.js";
 
 const router = express.Router();
 
 // Public routes
-router.post("/register", register);
-router.post("/login", login);
+router.post("/register", registerValidation, validate, register);
+router.post("/login", loginValidation, validate, login);
 
-//Protected routes
-router.get("/profil", getProfil);
+// Protected routes
+router.get("/profil", protect, getProfil);
 
 export default router;
